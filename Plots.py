914f849af2d0f93c6b74b73667c0475ecb20b5e6
emigu777 <emily.guillou1@gmail.com>
2022-08-03 16:33:14 +0200

    prettier Plots

--- Plots.py
+++ Plots.py
@@ -6,15 +6,18 @@ from helper import *
 class Plot():
     
     def __init__(self):
-        self.x = [10, 20, 30, 40]
+        self.x = [10, 15, 20]
+        self.x2 = [10.7, 15.7, 20.7]
         self.score = 0
         self.bflies = 0
-        self.highsc = 0
         self.pigeons = 0
+        self.highsc = 0
+        self.highb = 0
+        self.highp = 0
         self.name = ''
         self.last_row = []
         self.color = RED
-        self.tick_label = ["Score", "Butterflies", "Pigeons", "Highscore"]
+        self.tick_label = ["Score", "Butterflies", "Pigeons"]
         self.scoreboard = pd.read_csv('data\scoreboard.csv')
         print(self.scoreboard)
         self.fig, self.ax = plt.subplots(nrows = 1, ncols = 1)
@@ -27,11 +30,17 @@ class Plot():
         self.bflies = self.last_row[2]
         self.pigeons = self.last_row[3]
         self.highsc = max(self.scoreboard.iloc[:,1].tolist())
+        self.highb = max(self.scoreboard.iloc[:,2].tolist())
+        self.highp = max(self.scoreboard.iloc[:,3].tolist())
 
     #plots the personal score, the number of butterflies that were caught and the number of pigeons that were shot
-    #also plots the highscore
+    #also plots the highest scores
     def plot(self):
-        self.ax.bar(self.x, (self.score, self.bflies, self.pigeons, self.highsc),  tick_label = self.tick_label )
+        self.ax.bar(self.x, (self.score, self.bflies, self.pigeons), tick_label = self.tick_label,  color = '#77CEF0' )
+        print(self.highb)
+        self.ax.bar(self.x2, (self.highsc, self.highb, self.highp), color = '#77F077')
+        #self.ax.xticks(self.x, self.tick_label)
         self.ax.set_title('Scoreboard of ' + self.name)
+        self.ax.legend(["Your score", "Highscores"])
 
         plt.show()
